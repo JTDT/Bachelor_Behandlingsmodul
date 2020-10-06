578366fe2817e15b5ce0146de7f089eea386ac52
Jenny Tran <jenny.thuy.dung.tran@post.au.dk>
2020-10-06 19:55:48 +0200

    Hente målinger fra arduino

--- processRawData.m
+++ processRawData.m
@@ -1,12 +1,28 @@
 % Processing raw data from Arduino and pulsoximeter
-% Set up
-clear,clc
-% Connect to Arduino Hardware and include the I2C library
-a = arduino('COM3','Uno','Libraries','I2C');
-% Configure pins
-% configurePin(a,'D4','pullup');
-% pinMode = configurePin(a,'D4');
+%% Clean up
+clear a;
+clear,clc, close all
+%% Get pulse and O2 saturation
+comPort = serialportlist('available')
+arduino = serial(comPort(1),'BaudRate',115200);
+i=0;
+fopen(arduino);
+while (i<10)  % Should run continuosly?
+    fprintf(arduino,'Your serial data');
+    output = fscanf(arduino);  % Data type is char.
+    outputstr = convertCharsToStrings(output); % Convert to string
+    % get only values for Heart rate and 0xygen saturation
+    bioData = contains(outputstr,['Heartrate','Oxygen']);
+    if (bioData == 1)
+        sprintf(bioData)
+    end
+    i=i+1;
+end
+fclose(arduino);
 
+%% Connect to Arduino Hardware and include the I2C library
+port = 'COM3'; % check in device manager
+a = arduino(port,'Uno','Libraries','I2C');
 % Scans for available I2C addresses.
 addresses = scanI2CBus(a);
 % Create the I2C device object
@@ -14,17 +30,10 @@ i2c_device = device(a,'I2CAddress','0x55')
 % Read data 
 sensor_data = read(i2c_device,8,'uint8')
 
-%% Clean up
-clear a;
 
-%% Method 2
-comPort = serialportlist('available');
-arduino = serial(comPort,'BaudRate',115200);
-i=0;
-while (i<10)  % For X times execution
-    fopen(arduino);
-    fprintf(arduino,'Your serial data');
-    output = fscanf(arduino)  % output is displayed. Data type is char.
-    numout = str2double(output)   % For output in double data type.
-    i=i+1;
-end
+
+
+
+
+
+
