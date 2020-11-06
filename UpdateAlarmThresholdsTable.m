9b3ab45a3e34556bc8e039602e1f99262aa0933b
Celia <au582514@post.au.dk>
2020-11-06 16:13:02 +0100

    påbegyndt integrationstest UC8

---
+++ UpdateAlarmThresholdsTable.m
@@ -0,0 +1,13 @@
+function UpdateAlarmThresholdsTable(app, kk, HRmin, HRmax, SpO2min, SpO2max, time)
+% forklarende tekst 
+
+app.AlarmThresholdsTable {kk,1} = kk;
+app.AlarmThresholdsTable {kk,2} = HRmin;
+app.AlarmThresholdsTable {kk,3} = HRmax;
+app.AlarmThresholdsTable {kk,4} = SpO2min;
+app.AlarmThresholdsTable {kk,5} = SpO2max;
+app.AlarmThresholdsTable {kk,6} = time;
+
+app.AlarmThresholdsTable  % bruges til test -> skal slettes!!!
+
+end
