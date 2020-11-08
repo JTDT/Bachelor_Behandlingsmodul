cb867e0fc71aca685735e8c0631d944fb0cda745
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2020-11-08 14:54:26 +0100

    Ændringer i EditThresholdApp og AlarmThresholdsTable

--- UpdateAlarmThresholdsTable.m
+++ UpdateAlarmThresholdsTable.m
@@ -1,13 +1,14 @@
 function UpdateAlarmThresholdsTable(app, kk, HRmin, HRmax, SpO2min, SpO2max, time)
 % forklarende tekst 
 
-app.AlarmThresholdsTable {kk,1} = kk;
-app.AlarmThresholdsTable {kk,2} = HRmin;
-app.AlarmThresholdsTable {kk,3} = HRmax;
-app.AlarmThresholdsTable {kk,4} = SpO2min;
-app.AlarmThresholdsTable {kk,5} = SpO2max;
-app.AlarmThresholdsTable {kk,6} = time;
-disp(app.AlarmThresholdsTable{kk,6})
+app.AlarmThresholdsTable{kk,1} = kk;
+app.AlarmThresholdsTable{kk,2} = HRmin;
+app.AlarmThresholdsTable{kk,3} = HRmax;
+app.AlarmThresholdsTable{kk,4} = SpO2min;
+app.AlarmThresholdsTable{kk,5} = SpO2max;
+app.AlarmThresholdsTable{kk,6} = time;
+%disp(app.AlarmThresholdsTable{kk,6})
+disp(app.AlarmThresholdsTable)
 %app.A
 %%app.AlarmThresholdsTable  % bruges til test -> skal slettes!!!
 
