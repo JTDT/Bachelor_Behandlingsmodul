0af7f69db2aefef39aa37100b954bf03adda4ded
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2020-12-04 10:27:51 +0100

    Ændret [] til getHeight

--- takeScreenDump.m
+++ takeScreenDump.m
@@ -11,8 +11,8 @@ capture = robot.createScreenCapture(rectangle);
 
 %Convert to an RBG image
 %typecast(X,type)
-rgbimage = typecast(capture.getRGB(0,0,capture.getWidth,capture.getHeight,[],0,capture.getWidth),'uint8');
-img(:,:,1)=reshape(rgbimage(3:4:end),capture.getWidth,[])'; %specify a single dimension size of [] to have the dimension size automatically calculated
-img(:,:,2)=reshape(rgbimage(2:4:end),capture.getWidth,[])';
-img(:,:,3)=reshape(rgbimage(1:4:end),capture.getWidth,[])';
+rgbimage = typecast(capture.getRGB(0,0,capture.getWidth,capture.getHeight,[],0,capture.getWidth),'uint8'); %specify a single dimension size of [] to have the dimension size automatically calculated
+img(:,:,1)=reshape(rgbimage(3:4:end),capture.getWidth,capture.getHeight)'; 
+img(:,:,2)=reshape(rgbimage(2:4:end),capture.getWidth,capture.getHeight)';
+img(:,:,3)=reshape(rgbimage(1:4:end),capture.getWidth,capture.getHeight)';
 end 
