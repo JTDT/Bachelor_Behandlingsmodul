8cf29111ad7bc2165fce2f47b16b3d346bacff5d
theapkk1 <thea.plenus.kjeldahl.kristensen@post.au.dk>
2020-12-11 16:07:49 +0100

    Præallokkering af img i takeScreenDump

--- takeScreenDump.m
+++ takeScreenDump.m
@@ -12,6 +12,7 @@ capture = robot.createScreenCapture(rectangle);
 %Convert to an RBG image
 %typecast(X,type)
 rgbimage = typecast(capture.getRGB(0,0,capture.getWidth,capture.getHeight,[],0,capture.getWidth),'uint8'); %specify a single dimension size of [] to have the dimension size automatically calculated
+img =  zeros(capture.getHeight,capture.getWidth,3,'uint8');
 img(:,:,1)=reshape(rgbimage(3:4:end),capture.getWidth,capture.getHeight)'; 
 img(:,:,2)=reshape(rgbimage(2:4:end),capture.getWidth,capture.getHeight)';
 img(:,:,3)=reshape(rgbimage(1:4:end),capture.getWidth,capture.getHeight)';
